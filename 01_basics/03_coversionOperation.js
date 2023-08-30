57e99d28c3a55d901493f6fa5289f347364a98bf
Sumant Singh <singhsumant581@gmail.com>
2023-08-30 13:59:26 +0000

    conversion of Datatypes

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// 33 => "33"
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" =>false
+// "sumant" => true
+
+let SomeNumber = 33;
+
+let StringNumber = String(SomeNumber)
+
+console.log(StringNumber);
+console.log( typeof StringNumber);
